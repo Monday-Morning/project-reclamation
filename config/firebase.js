97fdfb3c359652dbefb06e53bb1df79c1a1acfd2
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2020-11-22 22:10:51 +0530

    Add firebase service account secret

--- config/firebase.js
+++ config/firebase.js
@@ -1,16 +1,5 @@
 const admin = require('firebase-admin');
-const firebaseServiceAccount = JSON.stringify({
-  type: 'service_account',
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
-  client_email: process.env.FIREBASE_CLIENT_ID,
-  client_id: process.env.CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
-});
+const firebaseServiceAccount = require('./firebase-service-account.json');
 
 try {
   admin.initializeApp({
