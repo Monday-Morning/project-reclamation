6bdc41f536cdf1baa8cb603f56dad348f509b255
ajibeshMohapatra <62394580+ajibeshMohapatra@users.noreply.github.com>
2020-12-23 13:33:58 +0530

    Used winston as a logger

--- config/firebase.js
+++ config/firebase.js
@@ -1,14 +1,15 @@
 const admin = require('firebase-admin');
 const firebaseServiceAccount = require('./firebase-service-account.json');
+const winston = require('./winston');
 
 try {
   admin.initializeApp({
     credential: admin.credential.cert(firebaseServiceAccount),
     storageBucket: process.env.GCP_STORAGE_BUCKET,
   });
-  console.info('Reclamation Server | Firebase | Admin Application Initialized');
+  winston.info('Reclamation Server | Firebase | Admin Application Initialized');
 } catch (e) {
-  console.error(new Error('Reclamation Server | Firebase | Could not initialize admin application'), e);
+  winston.error(new Error('Reclamation Server | Firebase | Could not initialize admin application'), e);
   return;
 }
 
