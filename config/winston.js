88d812395b01a63d750bddcf11953e0db529ee18
ajibeshMohapatra <62394580+ajibeshMohapatra@users.noreply.github.com>
2020-12-30 00:46:07 +0530

    final setup
     used env variable to switch between console and files

--- config/winston.js
+++ config/winston.js
@@ -1,40 +1,49 @@
 const { createLogger, format, transports } = require('winston');
 
-var options = {
-  error : {
-    level:'error',
-    filename:'./logs/error.log',
-    format: format.combine(
-      format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
-      format.printf(({level, message, timestamp}) => `[${timestamp}] [${level}]: ${message}`),
-    ),
-    handleExceptions: true,
-    maxsize: 5242880, // 5MB
-    maxFiles: 5,
-    json: true,
-    colorize: true,
-  },
-  combined : {
-    level:'info',
-    filename:'./logs/app.log',
-    format: format.combine(
-      format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
-      format.printf(({level, message, timestamp}) => `[${timestamp}] [${level}]: ${message}`),
-    ),
-    handleExceptions: true,
-    maxsize: 5242880, // 5MB
-    maxFiles: 5,
-    json: true,
-    colorize: true,
-  },
-}
-
-var logger = createLogger({
-  transports: [
-    new transports.File(options.error),
-    new transports.File(options.combined),
-  ],
-  exitOnError: false, 
+const logFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} | Project Reclamation | ${level.toUpperCase()} | ${message}`;
 });
+
+var options = {
+  error: {
+    level: 'error',
+    filename: './logs/error.log',
+    format: format.combine(format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), logFormat),
+    handleExceptions: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+    json: true,
+    colorize: true,
+  },
+  combined: {
+    level: 'info',
+    filename: './logs/app.log',
+    format: format.combine(format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), logFormat),
+    handleExceptions: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+    json: true,
+    colorize: true,
+  },
+  console: {
+    level: 'debug',
+    format: format.combine(format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), logFormat),
+    handleExceptions: true,
+    json: false,
+    colorize: true,
+  },
+};
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV !== 'production') {
+  var logger = createLogger({
+    transports: [new transports.Console(options.console)],
+    exitOnError: false,
+  });
+} else {
+  var logger = createLogger({
+    transports: [new transports.File(options.error), new transports.File(options.combined)],
+    exitOnError: false,
+  });
+}
 
 module.exports = logger;
