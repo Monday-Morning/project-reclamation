2daf6240d47cc4b6dd87901e6e46d3fa22b9c150
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2021-01-08 22:39:05 +0530

    Index issue and mmSession model

--- server/mdb/models/index.js
+++ server/mdb/models/index.js
@@ -13,6 +13,8 @@
  * @requires module:app.models.article
  * @requires module:app.models.comment
  * @requires module:app.models.reaction
+ * @requires module:app.models.issue
+ * @requires module:app.models.mmSession
  *
  * @version 0.1.0
  * @since 0.1.0
@@ -106,4 +108,20 @@ module.exports = {
    * @see module:app.models.reaction
    */
   ReactionModel: require('./reaction'),
+
+  /**
+   * @description Issue Model
+   * @constant
+   *
+   * @see module:app.models.issue
+   */
+  IssueModel: require('./issue'),
+
+  /**
+   * @description MMSession Model
+   * @constant
+   *
+   * @see module:app.models.mmSession
+   */
+  MMSessionModel: require('./mmSession'),
 };
