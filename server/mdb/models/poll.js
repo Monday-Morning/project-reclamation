5711a09430055cc9b8cae82c1b0c5a156a3e5441
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2021-01-11 23:53:54 +0530

    Add poll model

---
+++ server/mdb/models/poll.js
@@ -0,0 +1,97 @@
+/**
+ * @module app.models.poll
+ * @description Poll Model
+ *
+ * @requires mongoose.Schema
+ * @requires mongoose.model
+ *
+ * @version schema:v1
+ * @since 0.1.0
+ */
+
+const { Schema, model } = require('mongoose');
+
+/**
+ * @description The schema definition for Poll Model
+ * @constant PollSchema
+ *
+ * @type {Schema}
+ */
+const PollSchema = new Schema(
+  {
+    question: [
+      {
+        type: Object,
+        required: true,
+      },
+    ],
+    options: [
+      {
+        type: String,
+        required: true,
+        trim: true,
+      },
+    ],
+    optionsCount: [
+      {
+        type: Number,
+        required: true,
+        min: 0,
+      },
+    ],
+    totalVotes: {
+      type: Number,
+      required: false,
+      default: 0,
+      min: 0,
+    },
+    expiry: {
+      type: Date,
+      required: true,
+      min: new Date(Date.now()),
+    },
+    article: {
+      type: Schema.Types.ObjectId,
+      ref: 'Article',
+      required: false,
+    },
+    reponses: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: false,
+      default: null,
+    },
+    updatedBy: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: false,
+      default: null,
+    },
+    schemaVersion: {
+      type: Number,
+      required: true,
+      default: 1,
+      min: 1,
+    },
+  },
+  {
+    timestamps: true,
+    collection: 'polls',
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
+  }
+);
+
+/**
+ * @description Generated Poll Model
+ * @constant PollModel
+ *
+ * @type {model}
+ */
+module.exports = model('Poll', PollSchema);
