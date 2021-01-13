b60074081dd0c9d9ceb694accff7eb188b7cff15
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2021-01-13 22:35:41 +0530

    Add Forum Thread, Message, Update MM Session and Poll Model (#46)
    * Add forum thread model
    
    * Add thread message model
    
    * Add poll model
    
    * Index forum thread, message and poll model
    
    * Update poll to store user response in own document
    
    * Multiple article referencing single poll
    
    * Add static info to mm session model
    
    * Apply suggestions from code review
    
    Co-authored-by: Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
    
    Co-authored-by: Abel Mathew <notify.abelmathew@gmail.com>

---
+++ server/mdb/models/forumMessage.js
@@ -0,0 +1,82 @@
+/**
+ * @module app.models.forum.message
+ * @description Forum Message Model
+ *
+ * @requires mongoose.Schema
+ * @requires mongoose.model
+ *
+ * @version schema:v1
+ * @since 0.1.0
+ */
+
+const { Schema, model } = require('mongoose');
+
+/**
+ * @description The schema definition for Forum Message Model
+ * @constant ForumMessageSchema
+ *
+ * @type {Schema}
+ */
+const ForumMessageSchema = new Schema(
+  {
+    content: [
+      {
+        type: Object,
+        required: true,
+      },
+    ],
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    /** Additional permission required */
+    anonymous: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    validatedResponse: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    parentThread: {
+      type: Schema.Types.ObjectId,
+      ref: 'ForumThread',
+      required: true,
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: false,
+      default: null,
+    },
+    updatedBy: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: false,
+      default: null,
+    },
+    schemaVersion: {
+      type: Number,
+      required: true,
+      default: 1,
+      min: 1,
+    },
+  },
+  {
+    timestamps: true,
+    collection: 'forumMessages',
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
+  }
+);
+
+/**
+ * @description Generated Forum Message Model
+ * @constant ForumMessageModel
+ *
+ * @type {model}
+ */
+module.exports = model('ForumMessage', ForumMessageSchema);
