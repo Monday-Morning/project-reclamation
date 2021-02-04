7514f2f8c686a1332886d1e9d0443509bfeeb139
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2021-02-04 18:42:35 +0530

    Refactor article model

--- server/mdb/models/article.js
+++ server/mdb/models/article.js
@@ -19,8 +19,8 @@ const { Schema, model } = require('mongoose');
  */
 const ArticleSchema = new Schema(
   {
-    /** @enum [0 - Normal Article, 1 - Witsdom, 2 - Photostory] */
-    type: {
+    /** [0 - Normal Article, 1 - Witsdom, 2 - Photostory] */
+    articleType: {
       type: Number,
       required: true,
       min: 0,
@@ -31,6 +31,7 @@ const ArticleSchema = new Schema(
       required: true,
       trim: true,
     },
+    // TODO: update content with final structure
     content: [
       {
         type: Object,
@@ -42,64 +43,110 @@ const ArticleSchema = new Schema(
       required: false,
       trim: true,
     },
-    author: [
+    authors: [
       {
+        name: {
+          type: String,
+          required: true,
+        },
+        details: {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+          required: true,
+        },
+      },
+    ],
+    tech: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        details: {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+          required: true,
+        },
+      },
+    ],
+    category: {
+      subcategory: {
+        type: Boolean,
+        required: true,
+      },
+      number: {
+        type: Number,
+        required: true,
+        min: 0,
+      },
+      reference: {
         type: Schema.Types.ObjectId,
-        ref: 'User',
+        ref: 'CategoryMap',
         required: true,
       },
-    ],
-    category: [
-      {
-        type: Number,
-        required: true,
-        min: 0,
-      },
-    ],
-    subcategory: [
-      {
-        type: Number,
-        required: true,
-        min: 0,
-      },
-    ],
+    },
     tags: [
       {
-        type: Schema.Types.ObjectId,
-        ref: 'Tag',
-        required: true,
+        name: {
+          type: String,
+          required: true,
+          trim: true,
+        },
+        admin: {
+          type: Boolean,
+          required: false,
+          default: false,
+        },
+        reference: {
+          type: Schema.Types.ObjectId,
+          ref: 'Tag',
+          required: true,
+        },
       },
     ],
-    /** [Square Image, Rectangle Image] */
-    coverMedia: [
-      {
+    coverMedia: {
+      square: {
         type: Schema.Types.ObjectId,
         ref: 'Media',
         required: true,
       },
-    ],
-    /** @enum [0 - Unpublished, 1 - Published, 2 - Archive, 3 - Trash] */
+      rectangle: {
+        type: Schema.Types.ObjectId,
+        ref: 'Media',
+        required: true,
+      },
+    },
+    /** [0 - Unpublished, 1 - Published, 2 - Archive, 3 - Trash] */
     status: {
       type: Number,
       required: true,
       min: 0,
       max: 3,
     },
-    restrict: {
+    isLanRestricted: {
       type: Boolean,
       required: false,
       default: false,
     },
-    /**
-     * Object {
-     * 	reactions: Number,
-     * 	comments: Number,
-     * 	bookmarks: Number,
-     * }
-     */
     engagementCount: {
-      type: Object,
-      required: true,
+      reactions: {
+        type: Number,
+        required: false,
+        default: 0,
+        min: 0,
+      },
+      comments: {
+        type: Number,
+        required: false,
+        default: 0,
+        min: 0,
+      },
+      bookmarks: {
+        type: Number,
+        required: false,
+        default: 0,
+        min: 0,
+      },
     },
     /** For reads > 10% of readTime */
     views: {
@@ -140,7 +187,7 @@ const ArticleSchema = new Schema(
     },
     schemaVersion: {
       type: Number,
-      required: true,
+      required: false,
       default: 1,
       min: 1,
     },
