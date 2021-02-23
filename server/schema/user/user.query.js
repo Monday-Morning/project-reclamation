54d4f985fb2ddbeee93169318ee8a2bedf4ab995
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2021-02-24 01:07:25 +0530

    Add required queries

--- server/schema/user/user.query.js
+++ server/schema/user/user.query.js
@@ -10,13 +10,20 @@
 
 const {
   GraphQLObjectType,
-  // GraphQLString,
+  // GraphQLScalarType,
+  // GraphQLUnionType,
+  // GraphQLInputObjectType,
+  // GraphQLEnumType,
+  // GraphQLInterfaceType,
   // GraphQLSchema,
-  // GraphQLID,
-  // GraphQLList,
+  GraphQLNonNull,
+  // GraphQLError,
+  GraphQLList,
+  GraphQLString,
+  GraphQLID,
   // GraphQLBoolean,
   // GraphQLInt,
-  // GraphQLNonNull,
+  // GraphQLFloat,
   // GraphQLDate,
   // GraphQLTime,
   // GraphQLDateTime,
@@ -24,13 +31,37 @@ const {
   // GraphQLJSONObject,
 } = require('../scalars');
 
+const { getUser, listUsers, searchUsers } = require('./user.resolver');
+const { AccountTypeEnumType } = require('./user.enum.types');
+
 const UserType = require('./user.type');
 
 module.exports = new GraphQLObjectType({
   name: 'UserQuery',
   fields: {
-    user: {
+    getUser: {
       type: UserType,
+      args: {
+        id: { type: GraphQLID },
+        email: { type: GraphQLString },
+      },
+      resolve: getUser,
+    },
+    listUsers: {
+      type: new GraphQLList(UserType),
+      args: {
+        ids: { type: new GraphQLList(GraphQLID) },
+        emails: { type: new GraphQLList(GraphQLString) },
+      },
+      resolve: listUsers,
+    },
+    searchUsers: {
+      type: new GraphQLList(UserType),
+      args: {
+        searchParam: { type: new GraphQLNonNull(GraphQLString) },
+        accountType: { type: AccountTypeEnumType }, // Verified Type
+      },
+      resolve: searchUsers,
     },
   },
 });
