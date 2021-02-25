5105ba905a61b6ca11e774ac0babd99c2da376a4
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2021-02-25 18:41:11 +0530

    Document the user query

--- server/schema/user/user.query.js
+++ server/schema/user/user.query.js
@@ -40,26 +40,47 @@ module.exports = new GraphQLObjectType({
   name: 'UserQuery',
   fields: {
     getUser: {
+      description: 'Retrieves a single user.',
       type: UserType,
       args: {
-        id: { type: GraphQLID },
-        email: { type: GraphQLString },
+        id: {
+          description: "The user's mongo ID.",
+          type: GraphQLID,
+        },
+        email: {
+          description: "The user's email ID",
+          type: GraphQLString,
+        },
       },
       resolve: getUser,
     },
     listUsers: {
+      description: 'Retrieves a list of users.',
       type: new GraphQLList(UserType),
       args: {
-        ids: { type: new GraphQLList(GraphQLID) },
-        emails: { type: new GraphQLList(GraphQLString) },
+        ids: {
+          description: 'The list of user mongo IDs',
+          type: new GraphQLList(GraphQLID),
+        },
+        emails: {
+          description: 'The list of user email IDs',
+          type: new GraphQLList(GraphQLString),
+        },
       },
       resolve: listUsers,
     },
     searchUsers: {
+      description: 'Searches for a user using keywords.',
       type: new GraphQLList(UserType),
       args: {
-        searchParam: { type: new GraphQLNonNull(GraphQLString) },
-        accountType: { type: AccountTypeEnumType }, // Verified Type
+        keywords: {
+          description: 'The search keywords.',
+          type: new GraphQLNonNull(GraphQLString),
+        },
+        accountType: {
+          description: "The user's account type or verification status",
+          type: AccountTypeEnumType,
+        },
       },
       resolve: searchUsers,
     },
