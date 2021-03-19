568d4c0634fc98645238c1787dbc0f63f84f0313
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2021-03-20 03:04:48 +0530

    Add getUser resolver tests

--- test/schema/user.test.js
+++ test/schema/user.test.js
@@ -15,13 +15,121 @@ const {
   setUserBan,
 } = require('../../server/schema/user/user.resolver');
 
-describe('User Resolver Check', () => {
-  describe('getUser Function', () => {
+const UserModelMock = {
+  someUser: {
+    id: 'some-user',
+    firstName: 'John',
+    lastName: 'Doe',
+    picture: 'some-pic-id',
+    accountType: 0,
+    contributions: null,
+  },
+  someAuthor: {
+    id: 'some-author',
+    firstName: 'Jane',
+    lastName: 'Doe',
+    picture: 'some-author-pic-id',
+    accountType: 2,
+    contributions: [
+      { model: 'Article', reference: 'some-article-id' },
+      { model: 'Media', reference: 'some-media-id' },
+    ],
+  },
+  find: (params, fields, options) => {
+    if (params.id === 'some-user') {
+      return UserModelMock.someUser;
+    } else if (params.id === 'some-author') {
+      return UserModelMock.someAuthor;
+    } else {
+      return null;
+    }
+  },
+};
+
+describe('User Resolver Module', async () => {
+  describe('getUser Function', async () => {
     // case: does not have permission to read
+    it('Returns FORBIDDEN error if the user does not have permission to read', async () => {
+      const _response = await getUser(
+        null,
+        { id: 'some-user' },
+        {},
+        { fieldNodes: ['id', 'firstName', 'lastName', 'picture'] },
+        UserModelMock
+      );
+
+      expect(_response.name).to.be.equal('GraphQLError');
+      expect(_response.message).to.be.equal('FORBIDDEN');
+    });
+
     // case: does not have permission for particular field -> use info.fieldNodes
     // only name and pic is public for accTyp 0
+    it('Returns FORBIDDEN error if the user does not have permission to read a particular field', async () => {
+      const _response = await getUser(
+        null,
+        { id: 'some-user' },
+        { decodedToken: { customClaims: { roles: ['user.basic'] } } },
+        { fieldNodes: ['id', 'firstName', 'lastName', 'picture', 'contributions'] },
+        UserModelMock
+      );
+
+      expect(_response.name).to.be.equal('GraphQLError');
+      expect(_response.message).to.be.equal('FORBIDDEN');
+    });
+
     // case: user does not exist
+    it('Returns NOT FOUND error if the user does not exist', async () => {
+      const _response = await getUser(
+        null,
+        { id: 'some-non-existant-id' },
+        { decodedToken: { customClaims: { roles: ['user.basic'] } } },
+        { fieldNodes: ['id', 'firstName', 'lastName', 'picture'] },
+        UserModelMock
+      );
+
+      expect(_response.name).to.be.equal('GraphQLError');
+      expect(_response.message).to.be.equal('NOT_FOUND');
+    });
+
     // case: all works
+    it('Returns User object if successful with user.read.public permission', async () => {
+      const _response = await getUser(
+        null,
+        { id: 'some-user' },
+        { decodedToken: { customClaims: { roles: ['user.basic'] } } },
+        { fieldNodes: ['id', 'firstName', 'lastName', 'picture'] },
+        UserModelMock
+      );
+
+      let expectedData = UserModelMock.someUser;
+      delete expectedData.contributions;
+
+      expect(_response).to.deep.equal(expectedData);
+    });
+
+    it('Returns User object if successful with user.read.private permission', async () => {
+      const _response = await getUser(
+        null,
+        { id: 'some-user' },
+        { decodedToken: { customClaims: { roles: ['user.verified'] } } },
+        { fieldNodes: ['id', 'firstName', 'lastName', 'picture', 'contributions'] },
+        UserModelMock
+      );
+
+      expect(_response).to.deep.equal(UserModelMock.someUser);
+    });
+
+    it('Returns User object if account type is > 0', async () => {
+      const _response = await getUser(
+        null,
+        { id: 'some-author' },
+        {},
+        { fieldNodes: ['id', 'firstName', 'lastName', 'picture', 'contributions'] },
+        UserModelMock
+      );
+
+      expect(_response).to.deep.equal(UserModelMock.someAuthor);
+    });
   });
 
   describe('listUsers Function', () => {
