60480911bf8a98a5e7fb0ba2760f2363789a4548
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2021-03-20 03:04:59 +0530

    Add getUser resolver

--- server/schema/user/user.resolver.js
+++ server/schema/user/user.resolver.js
@@ -11,39 +11,74 @@
  */
 
 const { HasPermmission } = require('../../helpers/authorization');
+const { APIError, GraphQLError } = require('../../helpers/errorHandler');
 const UserModel = require('./user.model');
 
+const PUBLIC_FIELDS = [
+  'id',
+  'firstName',
+  'lastName',
+  'picture',
+  'pictureId',
+  'fullName',
+  'nitrMail',
+  'accountType',
+  'email',
+];
+
 module.exports = {
-  getUser: async (parent, args, context, info, _UserModel = UserModel, _HasPermission = HasPermmission) => {},
-  listUsers: async (parent, args, context, info, _UserModel = UserModel, _HasPermission = HasPermmission) => {},
-  searchUsers: async (parent, args, context, info, _UserModel = UserModel, _HasPermission = HasPermmission) => {},
+  getUser: async (parent, args, context, info, _UserModel = UserModel) => {
+    try {
+      if (!args || (!args.id && !args.email)) {
+        return APIError('BAD_REQUEST');
+      }
 
-  createUser: async (parent, args, context, info, _UserModel = UserModel, _HasPermission = HasPermmission) => {},
+      const _user = !args.id
+        ? await _UserModel.find({ email: args.email }, { lean: true })
+        : await _UserModel.find({ id: args.id }, { lean: true });
 
-  updateUserName: async (parent, args, context, info, _UserModel = UserModel, _HasPermission = HasPermmission) => {},
-  updateUserPicture: async (parent, args, context, info, _UserModel = UserModel, _HasPermission = HasPermmission) => {},
-  updateUserTopics: async (parent, args, context, info, _UserModel = UserModel, _HasPermission = HasPermmission) => {},
-  updateUserBio: async (parent, args, context, info, _UserModel = UserModel, _HasPermission = HasPermmission) => {},
+      if (!_user) {
+        return APIError('NOT_FOUND');
+      }
 
-  addNITRMail: async (parent, args, context, info, _UserModel = UserModel, _HasPermission = HasPermmission) => {},
-  verifyNITRMail: async (parent, args, context, info, _UserModel = UserModel, _HasPermission = HasPermmission) => {},
+      if (_user.accountType > 0) {
+        return _user;
+      }
 
-  newsletterSubscription: async (
-    parent,
-    args,
-    context,
-    info,
-    _UserModel = UserModel,
-    _HasPermission = HasPermmission
-  ) => {},
+      if (!HasPermmission(context, 'user.read.public')) {
+        return APIError('FORBIDDEN');
+      }
 
-  setUserVerfiedStatus: async (
-    parent,
-    args,
-    context,
-    info,
-    _UserModel = UserModel,
-    _HasPermission = HasPermmission
-  ) => {},
-  setUserBan: async (parent, args, context, info, _UserModel = UserModel, _HasPermission = HasPermmission) => {},
+      if (
+        info.fieldNodes.some((item) => !PUBLIC_FIELDS.includes(item)) &&
+        !HasPermmission(context, 'user.read.private')
+      ) {
+        return APIError('FORBIDDEN');
+      }
+
+      return _user;
+    } catch (e) {
+      if (e instanceof GraphQLError) {
+        return e;
+      }
+      return APIError(null, e);
+    }
+  },
+  listUsers: async (parent, args, context, info, _UserModel = UserModel) => {},
+  searchUsers: async (parent, args, context, info, _UserModel = UserModel) => {},
+
+  createUser: async (parent, args, context, info, _UserModel = UserModel) => {},
+
+  updateUserName: async (parent, args, context, info, _UserModel = UserModel) => {},
+  updateUserPicture: async (parent, args, context, info, _UserModel = UserModel) => {},
+  updateUserTopics: async (parent, args, context, info, _UserModel = UserModel) => {},
+  updateUserBio: async (parent, args, context, info, _UserModel = UserModel) => {},
+
+  addNITRMail: async (parent, args, context, info, _UserModel = UserModel) => {},
+  verifyNITRMail: async (parent, args, context, info, _UserModel = UserModel) => {},
+
+  newsletterSubscription: async (parent, args, context, info, _UserModel = UserModel) => {},
+
+  setUserVerfiedStatus: async (parent, args, context, info, _UserModel = UserModel) => {},
+  setUserBan: async (parent, args, context, info, _UserModel = UserModel) => {},
 };
