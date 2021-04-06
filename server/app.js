3d8b3d12ebb47e88188844dea27b3a33ea0e9e53
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2021-04-06 16:32:33 +0530

    Add and configure nodemailer

--- server/app.js
+++ server/app.js
@@ -28,6 +28,7 @@ const { ApolloServer } = require('apollo-server-express');
 const CORS = require('cors');
 const { init: firebaseInit } = require('./config/firebase');
 const { init: mongooseInit } = require('./config/mongoose');
+const { init: nodemailerInit } = require('./config/nodemailer');
 const { GetUserAuthScope, CacheRoles } = require('./helpers/authorization');
 const Winston = require('./helpers/winston');
 const logger = new Winston('app');
@@ -47,6 +48,7 @@ const router = require('./router');
 firebaseInit();
 mongooseInit();
 CacheRoles();
+nodemailerInit();
 
 /**
  * @summary Main Express Application
