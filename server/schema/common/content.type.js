402379c3472583603736d3a9cb1c205f65291fce
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2021-05-04 16:08:42 +0530

    Add content type

---
+++ server/schema/common/content.type.js
@@ -0,0 +1,76 @@
+const {
+  GraphQLObjectType,
+  // GraphQLScalarType,
+  GraphQLUnionType,
+  // GraphQLInputObjectType,
+  // GraphQLEnumType,
+  // GraphQLInterfaceType,
+  // GraphQLSchema,
+  GraphQLNonNull,
+  // GraphQLError,
+  GraphQLList,
+  GraphQLString,
+  GraphQLID,
+  GraphQLBoolean,
+  GraphQLInt,
+  // GraphQLFloat,
+  // GraphQLDate,
+  // GraphQLTime,
+  // GraphQLDateTime,
+  // GraphQLJSON,
+  GraphQLJSONObject,
+} = require('../scalars');
+const {
+  ContentTypeEnumType,
+  OrderedListStyleEnumType,
+  UnorderedListStyleEnumType,
+  AlignEnumType,
+} = require('./content.enum.types');
+
+const BlockFormattingType = new GraphQLObjectType({
+  name: 'BlockFormatting',
+  fields: () => ({
+    align: { type: AlignEnumType },
+    hasHeaderRow: { type: GraphQLBoolean },
+    listStyle: {
+      type: new GraphQLUnionType({
+        name: 'ListStyleUnion',
+        types: [OrderedListStyleEnumType, UnorderedListStyleEnumType],
+      }),
+    },
+  }),
+});
+
+const TextFormattingType = new GraphQLObjectType({
+  name: 'TextFormatting',
+  fields: () => ({
+    bold: { type: GraphQLBoolean },
+    italic: { type: GraphQLBoolean },
+    underline: { type: GraphQLBoolean },
+    strikethrough: { type: GraphQLBoolean },
+    size: { type: GraphQLInt },
+    start: { type: GraphQLInt },
+    end: { type: GraphQLInt },
+  }),
+});
+
+const ContentType = new GraphQLObjectType({
+  name: 'Content',
+  fields: () => ({
+    plaintext: { type: new GraphQLNonNull(GraphQLString) },
+    data: { type: GraphQLJSONObject },
+    mediaID: { type: GraphQLID },
+    // TODO: Resolve to MediaType
+    /*
+    media: {
+      type: MediaType,
+      resolve: async (parent, args, context, info) => {},
+    },
+		*/
+    contentType: { type: new GraphQLNonNull(ContentTypeEnumType) },
+    blockFormatting: { type: BlockFormattingType },
+    textFormatting: { type: new GraphQLList(TextFormattingType) },
+  }),
+});
+
+module.exports = ContentType;
