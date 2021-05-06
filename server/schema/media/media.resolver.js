7d8018d2ee2047e8c536d3f0587bd305f437e920
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2021-05-07 01:21:16 +0530

    Add getMedia resolver

---
+++ server/schema/media/media.resolver.js
@@ -0,0 +1,17 @@
+const MediaModel = require('./media.model');
+const { APIError } = require('../../helpers/errorHandler');
+
+module.exports = {
+  getMedia: async (_parent, { id }, _) => {
+    try {
+      const _media = await MediaModel.findById(id);
+      if (!_media) {
+        return APIError('NOT_FOUND');
+      }
+
+      return _media;
+    } catch (error) {
+      return APIError(null, error);
+    }
+  },
+};
