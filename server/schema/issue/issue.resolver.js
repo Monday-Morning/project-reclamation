741bdb335e83af223a13b0cda74918ff997eeb1a
Sriram Patibanda <patibandasriram2k02@gmail.com>
2021-05-06 21:34:25 +0530

    Adds resolvers for updateIssue and list Issue, adds permission checks for all the resolvers

--- server/schema/issue/issue.resolver.js
+++ server/schema/issue/issue.resolver.js
@@ -11,13 +11,15 @@
 
 const { GraphQLError, APIError, FirebaseAuthError } = require('../../helpers/errorHandler');
 const { Model } = require('mongoose');
-
+const { HasPermmission } = require('../../helpers/authorization');
 /**
  * @type {Model}
  */
 
 const IssueModel = require('./issue.model');
 
+const DEF_LIMIT = 10;
+const DEF_OFFSET = 0;
 module.exports = {
   getIssueByID: async (parent, { id }, context, info, _IssueModel = IssueModel) => {
     try {
@@ -26,15 +28,32 @@ module.exports = {
       if (!_issue) {
         return APIError('NOT_FOUND');
       }
+
+      if (_issue.publishedAt < Date.now() && !HasPermmission(context, 'issue.read.unpublished')) {
+        return APIError('NOT_FOUND');
+      }
+
       return _issue;
     } catch (e) {
       return APIError(null, e);
     }
   },
-  listIssues: async (parent, args, context, info, _IssueModel = IssueModel) => {},
-  addIssue: async (parent, { name, articles, featured }, context, info, _IssueModel = IssueModel) => {
+  listIssues: async (parent, { limit = DEF_LIMIT, offset = DEF_OFFSET }, context, info, _IssueModel = IssueModel) => {
     try {
-      const _issue = _IssueModel.create({ name, articles, featured });
+      const issueQuery = HasPermmission(context, 'issue.list.private') ? {} : { publishedAt: { $lt: Date.now() } };
+      const _issues = await _IssueModel.find(issueQuery).skip(offset).limit(limit);
+
+      return _issues;
+    } catch (e) {
+      return APIError(null, e);
+    }
+  },
+  addIssue: async (parent, { name, description, publishedAt }, context, info, _IssueModel = IssueModel) => {
+    try {
+      if (!HasPermmission(context, 'issue.write.new')) {
+        return APIError('FORBIDDEN');
+      }
+      const _issue = _IssueModel.create({ name, description, publishedAt: new Date(publishedAt) });
 
       return _issue;
     } catch (e) {
@@ -44,19 +63,53 @@ module.exports = {
       return APIError(null, e);
     }
   },
-  updateIssue: async (parents, args, context, info, _IssueModel = IssueModel) => {
+  updateIssue: async (
+    parent,
+    { id, name, description, publishedAt, createdBy },
+    context,
+    info,
+    _IssueModel = IssueModel
+  ) => {
     try {
+      const _issue = await _IssueModel.findById(id);
+
+      if (!_issue) {
+        return APIError('NOT_FOUND');
+      }
+
+      if (_issue.createdBy && !HasPermmission(context, 'issue.write.self')) {
+        return APIError('FORBIDDEN');
+      } else if (!HasPermmission(context, 'issue.write.all')) {
+        return APIError('FORBIDDEN');
+      }
+
+      const getUpdateObject = (propertiesObject) => {
+        const updateObject = {};
+        for (key in propertiesObject) {
+          if (propertiesObject[key]) {
+            updateObject[key] = propertiesObject[key];
+          }
+        }
+
+        return updateObject;
+      };
+      const updateIssue = getUpdateObject({ name, description, publishedAt: new Date(publishedAt) });
+
+      return _IssueModel.findByIdAndUpdate(id, updateIssue);
     } catch (e) {
       return APIError(null, e);
     }
   },
   deleteIssue: async (parents, { id }, context, info, _IssueModel = IssueModel) => {
     try {
-      if (!(await _IssueModel.exists(id))) {
+      const _issue = await _IssueModel.findById(id);
+      if (!_issue) {
         return APIError('NOT_FOUND');
       }
-      const _issue = await _IssueModel.findByIdAndDelete(id);
-      return _issue;
+      if (!HasPermmission(context, 'issue.write.all')) {
+        APIError('FORBIDDEN');
+      }
+      return _IssueModel.findByIdAndDelete(id);
     } catch (e) {
       return APIError(null, e);
     }
