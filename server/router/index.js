2e962860d136d67c8aa409749f7e7645041837c7
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2021-05-23 03:07:40 +0530

    Remove unused code from router

--- server/router/index.js
+++ server/router/index.js
@@ -11,71 +11,7 @@
  */
 
 const express = require('express');
-// const authController = require('../controllers/auth_controller');
-const { auth } = require('../config/firebase');
 const { CacheRoles } = require('../helpers/authorization');
-
-/**
- * @method checkUserAuth
- * @description Method to check user authentication status and validate using Firebase Admin SDK
- * @private
- * @async
- *
- * @param {Object} req Request Parameters
- * @param {Object} res Response Object
- * @param {Object} next Express Next Object
- *
- * @return {*}
- *
- * @todo Shift into auth controller
- *
- * @memberof module:app.router
- * @see module:app.firebase
- *
- * @version 0.1.0
- * @since 0.1.0
- */
-/* eslint-disable-next-line */
-const checkUserAuth = async (req, res, next) => {
-  if (!process.env.NODE_ENV) {
-    return next();
-  }
-  if (req.headers.authorization === undefined || req.headers.authorization === null) {
-    return res.json({
-      error: true,
-      code: 403,
-      data: 'No Authorization Credential',
-    });
-  } else if (
-    req.session.auth !== null &&
-    req.session.auth.key !== null &&
-    req.session.auth.flag &&
-    req.session.auth.key !== req.headers.authorization
-  ) {
-    return next();
-  }
-  if (req.headers.authorization === process.env.TEST_AUTH_KEY) {
-    req.session.auth.key = req.headers.authorization;
-    req.session.auth.flag = true;
-    return next();
-  }
-  req.session.auth = {
-    key: null,
-    flag: false,
-  };
-  try {
-    await auth.verifyIdToken(req.headers.authorization, true);
-  } catch (e) {
-    return res.json({
-      error: e,
-      code: 403,
-      data: e.message || 'Access Forbidden',
-    });
-  }
-  req.session.auth.key = req.headers.authorization;
-  req.session.auth.flag = true;
-  return next();
-};
 
 /**
  * @summary Express Router Object
@@ -86,14 +22,8 @@ const checkUserAuth = async (req, res, next) => {
  */
 const router = express.Router();
 
-/** Authentication APIs */
-// router.post('/v1/auth/local', authController.local);
-// router.post('/v1/auth/google', authController.google);
-
-// router.post('/v1/auth/session', authController.start);
-// router.delete('/v1/auth/session', checkUserAuth, authController.end);
-
-router.use('/admin/roles/sync', async (req, res) => res.send(await CacheRoles()));
+/** Updates roles cache */
+router.use('/admin/roles/sync', async (_req, res) => res.send(await CacheRoles()));
 
 /** 404 Not Found - Default Response for Invalid Path */
 router.use((req, res) => {
