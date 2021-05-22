4eb4dd3e6859583a0d4f562af33f03ae8ed13ca4
Sriram Patibanda <patibandasriram2k02@gmail.com>
2021-05-22 21:12:36 +0530

    Add squiggle type to squiggle schema

---
+++ server/schema/squiggle/squiggle.type.js
@@ -0,0 +1,42 @@
+/**
+ * @module app.schema.SquiggleType
+ * @description SquiggleType
+ *
+ * @requires module:app.schema.scalars
+ *
+ * @version v1
+ * @since 0.1.0
+ */
+
+const {
+  GraphQLObjectType,
+  // GraphQLString,
+  // GraphQLSchema,
+  GraphQLID,
+  GraphQLList,
+  // GraphQLBoolean,
+  GraphQLInt,
+  // GraphQLNonNull,
+  // GraphQLDate,
+  // GraphQLTime,
+  GraphQLDateTime,
+  // GraphQLJSON,
+  // GraphQLJSONObject,
+} = require('../scalars');
+
+const ContentType = require('../common/content.type');
+
+const SquiggleType = new GraphQLObjectType({
+  name: 'Squiggle',
+  fields: () => ({
+    id: { type: GraphQLID },
+    content: { type: GraphQLList(ContentType) },
+    createdAt: { type: GraphQLDateTime },
+    createdBy: { type: GraphQLID },
+    updatedAt: { type: GraphQLDateTime },
+    updatedBy: { type: GraphQLID },
+    schemaVersion: { type: GraphQLInt },
+  }),
+});
+
+module.exports = SquiggleType;
