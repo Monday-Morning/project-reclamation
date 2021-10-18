1cafe010a5fe51da9f4fe93bdbfab5856524138d
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2021-10-19 03:11:53 +0530

    Refactor apollo server configuration

--- server/app.js
+++ server/app.js
@@ -22,16 +22,13 @@
 const Express = require('express');
 const Session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(Session);
-const { ApolloServer } = require('apollo-server-express');
-// const CookieParser = require('cookie-parser');
-// const CSRF = require('csurf');
 const CORS = require('cors');
 const { init: firebaseInit } = require('./config/firebase');
 const { init: mongooseInit } = require('./config/mongoose');
-const { init: nodemailerInit } = require('./config/nodemailer');
-const { GetUserAuthScope, CacheRoles } = require('./helpers/authorization');
-const Winston = require('./helpers/winston');
-const logger = new Winston('app');
+const { cache: cacheRoles } = require('./utils/userAuth/role');
+// const { init: nodemailerInit } = require('./config/nodemailer');
+const apolloServer = require('./config/apolloServer');
+const logger = require('./utils/logger')('app');
 
 /**
  * @summary Express Router Object
@@ -44,11 +41,11 @@ const logger = new Winston('app');
  */
 const router = require('./router');
 
-/** Initialize Mongoose, Firebase and Cache Roles */
+/** Initialize all libraries */
 firebaseInit();
 mongooseInit();
-CacheRoles();
-nodemailerInit();
+cacheRoles();
+// nodemailerInit();
 
 /**
  * @summary Main Express Application
@@ -59,8 +56,6 @@ nodemailerInit();
  */
 const app = Express();
 
-const DEFAULT_PORT = 8080;
-
 /**
  * @description Server Port
  * @constant PORT
@@ -68,7 +63,7 @@ const DEFAULT_PORT = 8080;
  * @type {Number}
  * @default 8080
  */
-const PORT = process.env.PORT || DEFAULT_PORT;
+const PORT = process.env.PORT || 8080;
 
 /**
  * @summary Cross Origin Options
@@ -79,41 +74,41 @@ const PORT = process.env.PORT || DEFAULT_PORT;
  * @default http://localhost:3000
  */
 const CORS_OPTIONS = {
-  // origin:
-  //   !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
-  //     ? ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:5000']
-  //     : process.env.NODE_ENV === 'staging'
-  //     ? ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:5000', 'https://mm.server1.dashnet.in']
-  //     : ['https://mondaymorning.nitrkl.ac.in', 'https://mondaymorning.nitrkl.in'],
+  credentials: true,
   origin(origin, callback) {
     if (
       (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') &&
-      (!origin || origin.includes('http://localhost'))
+      (!origin ||
+        origin.includes('http://localhost') ||
+        origin.includes('http://127.0.0.1') ||
+        origin.includes('apollographql.com'))
     ) {
       callback(null, true);
     } else if (
       process.env.NODE_ENV === 'staging' &&
       (!origin ||
         origin.includes('http://localhost') ||
+        origin.includes('http://127.0.0.1') ||
         origin.includes('https://project-infinity-98561') ||
-        origin.includes('https://mm.server1.dashnet.in'))
+        origin.includes('https://mm.server1.dashnet.in') ||
+        origin.includes('https://studio.apollographql.com'))
     ) {
       callback(null, true);
-    } else if (['https://mondaymorning.nitrkl.ac.in', 'https://mondaymorning.nitrkl.in'].includes(origin)) {
+    } else if (
+      [
+        'https://mondaymorning.nitrkl.ac.in',
+        'https://mondaymorning.nitrkl.in',
+        'https://studio.apollographql.com',
+      ].includes(origin)
+    ) {
       callback(null, true);
     } else {
+      logger.warn(`CORS blocked a request from ${origin}`);
       callback(new Error('Request blocked by CORS. Invalid source!'));
     }
   },
 };
 app.use(CORS(CORS_OPTIONS));
-
-/** Use Cookie Parse, JSON and Encoded URL Body Parser, and CSURF in Express */
-/** 
- * Disabled CSURF as Authorization: Bearer<> header is not vulnerable
- app.use(CookieParser());
- app.use(CSRF({ cookie: true }));
- */
 
 /** Use Error Handler in development environment */
 if (!process.env.NODE_ENV || process.env.NODE_ENV !== 'production') {
@@ -127,7 +122,8 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV !== 'production') {
       secret: process.env.SESSION_SECRET,
       key: process.env.SESSION_KEY,
       resave: false,
-      saveUninitialized: true,
+      saveUninitialized: false,
+      cookie: { secure: false, maxAge: 24 * 60 * 60 * 1000 },
     })
   );
 } else {
@@ -143,9 +139,8 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV !== 'production') {
     collection: 'sessionCacheStore',
     expires: 3600000, // 1 Hour
     connectionOptions: {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      poolSize: 100,
+      maxPoolSize: 100,
+      minPoolSize: 10,
       serverSelectionTimeoutMS: 10000,
     },
   });
@@ -160,57 +155,30 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV !== 'production') {
         maxAge: 86400000,
       },
       store,
-      resave: true,
-      saveUninitialized: true,
+      resave: false,
+      saveUninitialized: false,
     })
   );
 }
 
-/**
- * @summary Main Apollo Server
- * @description initialize Apollo server with required configration and attach schema
- * @constant apolloServer
- *
- * @type {ApolloServer}
- */
-const apolloServer = new ApolloServer({
-  schema: require('./schema'),
-  context: async ({ req }) => ({
-    authToken: req.headers && req.headers.authorization ? decodeURI(req.headers.authorization) : null,
-    // csrfToken: req.csrfToken(), // Disabled CSURF
-    decodedToken:
-      req.headers && req.headers.authorization
-        ? await GetUserAuthScope(req.session, decodeURI(req.headers.authorization))
-        : null,
-    mid: async () => {
-      const decodedToken =
-        req.headers && req.headers.authorization
-          ? await GetUserAuthScope(req.session, decodeURI(req.headers.authorization))
-          : null;
-      return decodedToken ? decodedToken.customClaims.mid : null;
-    },
-    session: req.session,
-  }),
-  cors: CORS_OPTIONS,
-  playground: !process.env.NODE_ENV || process.env.NODE_ENV !== 'production',
-  debug: !process.env.NODE_ENV || process.env.NODE_ENV === 'development',
-  tracing: !process.env.NODE_ENV || process.env.NODE_ENV !== 'production',
-});
-
-/** Attach Express Server with Apollo Server */
-apolloServer.applyMiddleware({ app, path: '/v1/graph', cors: CORS_OPTIONS });
-
-/** Attach Express Router */
-app.use(router);
-
-/** Start Express Server on defined port */
-app.listen(PORT, (err) => {
-  if (err) {
-    logger.error(`Express Server Error on Port ${PORT}`, err);
-    return;
-  }
-  logger.info(`Express Server Started on Port ${PORT}`);
-});
+/** Start Apollo Server and attach to Express Server */
+apolloServer
+  .start()
+  .then(() => apolloServer.applyMiddleware({ app, path: '/v1/graph', cors: CORS_OPTIONS }))
+  /** Attach Express Router */
+  .then(() => app.use(router))
+  /** Start Express Server on defined port */
+  .then(() =>
+    app.listen(PORT, (err) => {
+      if (err) {
+        logger.error(`Could not start Express Server on Port ${PORT}: `, err);
+        return;
+      }
+      logger.info(`Express Server Started on Port ${PORT}.`);
+    })
+  )
+  .then(() => logger.info(`Apollo Server started and attached at ${apolloServer.graphqlPath}`))
+  .catch((err) => logger.error(`Could not start Apollo Server: `, err));
 
 /**
  * @description Main Express Application
