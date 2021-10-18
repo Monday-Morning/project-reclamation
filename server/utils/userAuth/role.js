2e104d2336d3d449bdd18c5d536858553fa6a4a6
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2021-10-19 04:30:57 +0530

    Refactor authentication and authorization

---
+++ server/utils/userAuth/role.js
@@ -0,0 +1,47 @@
+const RoleModel = require('../../schema/role/role.model');
+const logger = require('../logger')('utils.auth');
+const fs = require('fs');
+const { APIError } = require('../exception');
+let rolesCacheFile = './roles.json';
+
+const UserRole = {
+  /**
+   * @description Retrieves and caches all roles locally
+   * @function
+   * @async
+   *
+   * @param {RoleModel} _RoleModel
+   * @returns {NULL | GraphQLError} roles
+   */
+  cache: async (_RoleModel = RoleModel) => {
+    try {
+      const _roles = await _RoleModel.find({}, 'name permissions section', { lean: true });
+      fs.writeFileSync('./roles.json', JSON.stringify(_roles));
+      rolesCacheFile = fs.realpathSync('./roles.json');
+      return rolesCacheFile;
+    } catch (error) {
+      logger.error('The server could not update the roles cache.', error);
+      throw APIError(null, error, { reason: 'The server could not update the roles cache.' });
+    }
+  },
+
+  /**
+   * @description Retrieves all roles
+   * @function
+   *
+   * @returns {Array<Object> | GraphQLError}
+   */
+  get: () => {
+    try {
+      if (rolesCacheFile === './roles.json') {
+        rolesCacheFile = fs.realpathSync('./server/roles.json');
+      }
+      return JSON.parse(fs.readFileSync(rolesCacheFile));
+    } catch (error) {
+      logger.error('The server could not read the roles cache.', error);
+      throw APIError(null, error, { reason: 'The server could not read the roles cache.' });
+    }
+  },
+};
+
+module.exports = UserRole;
---
+++ server/utils/userAuth/session.js
@@ -0,0 +1,79 @@
+const { admin } = require('../../config/firebase');
+const UserAuth = require('.');
+const { APIError } = require('../exception');
+
+const UserSession = {
+  /**
+   * @description Checks if user's session is active and valid
+   * @function
+   *
+   * @param {session.Session} session
+   * @param {String} jwt
+   * @returns {Boolean}
+   */
+  valid: (session, jwt) =>
+    !session ||
+    !jwt ||
+    !session.auth ||
+    !session.auth.jwt ||
+    !session.auth.exp ||
+    !session.auth.roles ||
+    session.auth.jwt !== jwt ||
+    session.auth.exp <= Date.now() / 1000
+      ? false
+      : true,
+
+  /**
+   * @description Creates a session for a user and authenticates the user
+   * @function
+   * @async
+   *
+   * @param {session.Session} session
+   * @param {String} jwt
+   * @param {auth} _auth Firebase Authentication Library
+   * @returns {Object | GraphQLError} decodedToken
+   */
+  start: async (session, jwt, _auth = admin.auth()) => {
+    try {
+      const _decodedToken = await UserAuth.authenticate(jwt, _auth);
+      const { uid, exp, roles, mid } = _decodedToken;
+      session.auth = {
+        uid,
+        mid,
+        jwt,
+        exp,
+        roles,
+        decodedToken: _decodedToken,
+      };
+      await session.save();
+      return _decodedToken;
+    } catch (error) {
+      throw APIError(null, error, {
+        reason: 'The server was unable to initialize a session.',
+      });
+    }
+  },
+
+  /**
+   * @description Ends the running session
+   * @function
+   * @async
+   *
+   * @param {session.Session} session
+   * @param {String} jwt
+   * @returns {NULL | GraphQLError}
+   */
+  end: async (session, jwt) => {
+    try {
+      if (UserSession.valid(session, jwt)) {
+        await session.destroy();
+        return true;
+      }
+      return false;
+    } catch (error) {
+      throw APIError(null, error, { reason: "The server was unable to terminate the user's session." });
+    }
+  },
+};
+
+module.exports = UserSession;
