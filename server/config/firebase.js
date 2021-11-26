8f62e49340facb170e24bf2543332162c4aa6d11
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2021-11-26 22:40:36 +0530

    fix: encode firebase creds

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -20,20 +20,10 @@ module.exports = {
   init: () => {
     try {
       /** Inititalize Firebase Admin SDK with required configuration */
-      const firebaseServiceAccount = {
-        type: 'service_account',
-        project_id: process.env.FIREBASE_PROJECT_ID,
-        private_key_id: process.env.FIREBASE_KEY_ID,
-        private_key: process.env.FIREBASE_KEY,
-        client_email: process.env.FIREBASE_CLIENT_EMAIL,
-        client_id: process.env.FIREBASE_CLIENT_ID,
-        auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-        token_uri: 'https://oauth2.googleapis.com/token',
-        auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-        client_x509_cert_url:
-          'https://www.googleapis.com/robot/v1/metadata/x509/' + encodeURI(process.env.FIREBASE_CLIENT_EMAIL),
-      };
-      if (process.env.FIREBASE_PROJECT_ID) {
+      const firebaseServiceAccount = JSON.parse(
+        Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, 'base64').toString('ascii')
+      );
+      if (process.env.FIREBASE_SERVICE_ACCOUNT) {
         Admin.initializeApp({
           credential: Admin.credential.cert(firebaseServiceAccount),
           storageBucket: process.env.FIREBASE_STORAGE_BUCKET || null,
