76ffb6c09f738b68a2b183372ad0f87464736218
Aashish <83752052+120EE0692@users.noreply.github.com>
2022-11-05 14:41:46 +0530

    chore: change to getAutoComplete
    Co-authored-by: Rutaj Dash <33367546+rutajdash@users.noreply.github.com>

--- server/schema/article/article.query.js
+++ server/schema/article/article.query.js
@@ -25,7 +25,7 @@ const {
   getListOfArticles,
   getArticlesByCategories,
   searchArticle,
-  autoComplete,
+  getAutoComplete,
   listArticlesByYearAndMonth,
   listAllArticles,
   countOfArticlesBySubCategory,
