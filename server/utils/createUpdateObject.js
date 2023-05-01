39ed8372d351496cd01297df9f619cf76b59137f
Aashish <ashishpradhan2345@gmail.com>
2023-05-01 19:48:54 +0530

    fix: handle edge case in create update object

--- server/utils/createUpdateObject.js
+++ server/utils/createUpdateObject.js
@@ -1,7 +1,7 @@
 module.exports = (oldObject) => {
   const _newObject = {};
   for (const _key in oldObject) {
-    if (oldObject[_key]) {
+    if (oldObject[_key] || typeof oldObject[_key] === 'boolean' || typeof oldObject[_key] === 'number') {
       _newObject[_key] = oldObject[_key];
     }
   }
