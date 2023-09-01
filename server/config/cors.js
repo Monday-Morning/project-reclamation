cf42354763a869c5e6fcd0a3c99e749081171034
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2023-09-01 23:49:35 +0530

    feat(cors): convert to env variable

--- server/config/cors.js
+++ server/config/cors.js
@@ -1,26 +1,10 @@
 const CORS = require('cors');
 const logger = require('../utils/logger')('CORS');
 
-const ORIGIN_PATTERS = {
-  // Allows localhost, Apollo Studio
-  development: new RegExp(/^https?:\/\/((127\.0\.0\.1|localhost)(:\d{1,})?|studio\.apollographql\.com)$/),
-
-  // Allows localhost, Apollo Studio, DashNet MM Domains, NITR MM Domains, Render Staging Domains and PR Review Apps
-  staging: new RegExp(
-    /^https?:\/\/((127\.0\.0\.1|localhost)(:\d{1,})?|studio\.apollographql\.com|mm(\.server1)?\.dashnet\.in|mondaymorning\.nitrkl\.ac\.in|project-(reclamation|tahiti)-staging\.onrender\.com)$/
-  ),
-
-  // Allows Apollo Studio, DashNet MM Domains, and NITR MM Domains
-  // Does not allow localhost, Render Staging Domains and PR Review Apps
-  production: new RegExp(
-    /^https?:\/\/(studio\.apollographql\.com|mm(\.server1)?\.dashnet\.in|mondaymorning\.nitrkl\.ac\.in)$/
-  ),
-};
-
 const CORS_OPTIONS = {
   credentials: true,
   origin(origin, callback) {
-    if (!origin || origin.match(ORIGIN_PATTERS[process.env.NODE_ENV || 'production'])) {
+    if (!origin || origin.match(new RegExp(Buffer.from(process.env.CORS_ORIGIN_REGEX, 'base64').toString()))) {
       return callback(null, true);
     }
     logger.warn(`CORS blocked a request from ${origin}`);
@@ -28,4 +12,4 @@ const CORS_OPTIONS = {
   },
 };
 
-module.exports = { cors: CORS(CORS_OPTIONS), CORS_OPTIONS, ORIGIN_PATTERS };
+module.exports = { cors: CORS(CORS_OPTIONS), CORS_OPTIONS };
